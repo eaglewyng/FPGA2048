190713c34c2b3f1ad298a164c12ff9b5bef24c9e
Parker Ridd <parker.ridd@byu.net>
2014-04-02 12:27:30 -0600

    Added a comment

--- grid.vhd
+++ grid.vhd
@@ -33,265 +33,271 @@ signal boxValues: value;
 
 begin
 
-Box1 : entity work.Box 
+	--============================================================================
+	------------------Entity Instantiations---------------------------------------
+	--============================================================================
+	Box1 : entity work.Box 
 
-generic map(
-					XPOS => 128,
-					YPOS => 48
-				)
-				
-port map(
-				pixel_x => pixel_x,
-				pixel_y => pixel_y,
-				value => boxValues(0),
-				rgb_color => rgb1,
-				--this signal is so we can tell whether we should actually draw the color
-				--being output by the box or not!
-				drawBox => drawBox1
-);
+	generic map(
+						XPOS => 128,
+						YPOS => 48
+					)
+					
+	port map(
+					pixel_x => pixel_x,
+					pixel_y => pixel_y,
+					value => boxValues(0),
+					rgb_color => rgb1,
+					--this signal is so we can tell whether we should actually draw the color
+					--being output by the box or not!
+					drawBox => drawBox1
+	);
 
-Box2 : entity work.Box 
+	Box2 : entity work.Box 
 
-generic map(
-					XPOS => 226,
-					YPOS => 48
-				)
-port map(
-				pixel_x => pixel_x,
-				pixel_y => pixel_y,
-				value => boxValues(1),
-				rgb_color => rgb2,
-				--this signal is so we can tell whether we should actually draw the color
-				--being output by the box or not!
-				drawBox => drawBox2
-);
+	generic map(
+						XPOS => 226,
+						YPOS => 48
+					)
+	port map(
+					pixel_x => pixel_x,
+					pixel_y => pixel_y,
+					value => boxValues(1),
+					rgb_color => rgb2,
+					--this signal is so we can tell whether we should actually draw the color
+					--being output by the box or not!
+					drawBox => drawBox2
+	);
 
-Box3 : entity work.Box 
+	Box3 : entity work.Box 
 
-generic map(
-					XPOS => 324,
-					YPOS => 48
-				)
-port map(
-				pixel_x => pixel_x,
-				pixel_y => pixel_y,
-				value => boxValues(2),
-				rgb_color => rgb3,
-				--this signal is so we can tell whether we should actually draw the color
-				--being output by the box or not!
-				drawBox => drawBox3
-);
+	generic map(
+						XPOS => 324,
+						YPOS => 48
+					)
+	port map(
+					pixel_x => pixel_x,
+					pixel_y => pixel_y,
+					value => boxValues(2),
+					rgb_color => rgb3,
+					--this signal is so we can tell whether we should actually draw the color
+					--being output by the box or not!
+					drawBox => drawBox3
+	);
 
-Box4 : entity work.Box 
+	Box4 : entity work.Box 
 
-generic map(
-					XPOS => 422,
-					YPOS => 48
-				)
-port map(
-				pixel_x => pixel_x,
-				pixel_y => pixel_y,
-				value => boxValues(3),
-				rgb_color => rgb4,
-				--this signal is so we can tell whether we should actually draw the color
-				--being output by the box or not!
-				drawBox => drawBox4
-);
+	generic map(
+						XPOS => 422,
+						YPOS => 48
+					)
+	port map(
+					pixel_x => pixel_x,
+					pixel_y => pixel_y,
+					value => boxValues(3),
+					rgb_color => rgb4,
+					--this signal is so we can tell whether we should actually draw the color
+					--being output by the box or not!
+					drawBox => drawBox4
+	);
 
-Box5 : entity work.Box 
+	Box5 : entity work.Box 
 
-generic map(
-					XPOS => 128,
-					YPOS => 146
-				)
-port map(
-				pixel_x => pixel_x,
-				pixel_y => pixel_y,
-				value => boxValues(4),
-				rgb_color => rgb5,
-				--this signal is so we can tell whether we should actually draw the color
-				--being output by the box or not!
-				drawBox => drawBox5
-);
+	generic map(
+						XPOS => 128,
+						YPOS => 146
+					)
+	port map(
+					pixel_x => pixel_x,
+					pixel_y => pixel_y,
+					value => boxValues(4),
+					rgb_color => rgb5,
+					--this signal is so we can tell whether we should actually draw the color
+					--being output by the box or not!
+					drawBox => drawBox5
+	);
 
-Box6 : entity work.Box 
+	Box6 : entity work.Box 
 
-generic map(
-					XPOS => 226,
-					YPOS => 146
-				)
-port map(
-				pixel_x => pixel_x,
-				pixel_y => pixel_y,
-				value => boxValues(5),
-				rgb_color => rgb6,
-				--this signal is so we can tell whether we should actually draw the color
-				--being output by the box or not!
-				drawBox => drawBox6
-);
+	generic map(
+						XPOS => 226,
+						YPOS => 146
+					)
+	port map(
+					pixel_x => pixel_x,
+					pixel_y => pixel_y,
+					value => boxValues(5),
+					rgb_color => rgb6,
+					--this signal is so we can tell whether we should actually draw the color
+					--being output by the box or not!
+					drawBox => drawBox6
+	);
 
-Box7 : entity work.Box 
+	Box7 : entity work.Box 
 
-generic map(
-					XPOS => 324,
-					YPOS => 146
-				)
-port map(
-				pixel_x => pixel_x,
-				pixel_y => pixel_y,
-				value => boxValues(6),
-				rgb_color => rgb7,
-				--this signal is so we can tell whether we should actually draw the color
-				--being output by the box or not!
-				drawBox => drawBox7
-);
+	generic map(
+						XPOS => 324,
+						YPOS => 146
+					)
+	port map(
+					pixel_x => pixel_x,
+					pixel_y => pixel_y,
+					value => boxValues(6),
+					rgb_color => rgb7,
+					--this signal is so we can tell whether we should actually draw the color
+					--being output by the box or not!
+					drawBox => drawBox7
+	);
 
-Box8 : entity work.Box 
+	Box8 : entity work.Box 
 
-generic map(
-					XPOS => 422,
-					YPOS => 146
-				)
-port map(
-				pixel_x => pixel_x,
-				pixel_y => pixel_y,
-				value => boxValues(7),
-				rgb_color => rgb8,
-				--this signal is so we can tell whether we should actually draw the color
-				--being output by the box or not!
-				drawBox => drawBox8
-);
+	generic map(
+						XPOS => 422,
+						YPOS => 146
+					)
+	port map(
+					pixel_x => pixel_x,
+					pixel_y => pixel_y,
+					value => boxValues(7),
+					rgb_color => rgb8,
+					--this signal is so we can tell whether we should actually draw the color
+					--being output by the box or not!
+					drawBox => drawBox8
+	);
 
-Box9 : entity work.Box 
+	Box9 : entity work.Box 
 
-generic map(
-					XPOS => 128,
-					YPOS => 244
-				)
-port map(
-				pixel_x => pixel_x,
-				pixel_y => pixel_y,
-				value => boxValues(8),
-				rgb_color => rgb9,
-				--this signal is so we can tell whether we should actually draw the color
-				--being output by the box or not!
-				drawBox => drawBox9
-);
+	generic map(
+						XPOS => 128,
+						YPOS => 244
+					)
+	port map(
+					pixel_x => pixel_x,
+					pixel_y => pixel_y,
+					value => boxValues(8),
+					rgb_color => rgb9,
+					--this signal is so we can tell whether we should actually draw the color
+					--being output by the box or not!
+					drawBox => drawBox9
+	);
 
-Box10 : entity work.Box 
+	Box10 : entity work.Box 
 
-generic map(
-					XPOS => 226,
-					YPOS => 244
-				)
-port map(
-				pixel_x => pixel_x,
-				pixel_y => pixel_y,
-				value => boxValues(9),
-				rgb_color => rgb10,
-				--this signal is so we can tell whether we should actually draw the color
-				--being output by the box or not!
-				drawBox => drawBox10
-);
+	generic map(
+						XPOS => 226,
+						YPOS => 244
+					)
+	port map(
+					pixel_x => pixel_x,
+					pixel_y => pixel_y,
+					value => boxValues(9),
+					rgb_color => rgb10,
+					--this signal is so we can tell whether we should actually draw the color
+					--being output by the box or not!
+					drawBox => drawBox10
+	);
 
-Box11 : entity work.Box 
+	Box11 : entity work.Box 
 
-generic map(
-					XPOS => 324,
-					YPOS => 244
-				)
-port map(
-				pixel_x => pixel_x,
-				pixel_y => pixel_y,
-				value => boxValues(10),
-				rgb_color => rgb11,
-				--this signal is so we can tell whether we should actually draw the color
-				--being output by the box or not!
-				drawBox => drawBox11
-);
+	generic map(
+						XPOS => 324,
+						YPOS => 244
+					)
+	port map(
+					pixel_x => pixel_x,
+					pixel_y => pixel_y,
+					value => boxValues(10),
+					rgb_color => rgb11,
+					--this signal is so we can tell whether we should actually draw the color
+					--being output by the box or not!
+					drawBox => drawBox11
+	);
 
-Box12 : entity work.Box 
+	Box12 : entity work.Box 
 
-generic map(
-					XPOS => 422,
-					YPOS => 244
-				)
-port map(
-				pixel_x => pixel_x,
-				pixel_y => pixel_y,
-				value => boxValues(11),
-				rgb_color => rgb12,
-				--this signal is so we can tell whether we should actually draw the color
-				--being output by the box or not!
-				drawBox => drawBox12
-);
-
-
-Box13 : entity work.Box 
-
-generic map(
-					XPOS => 128,
-					YPOS => 342
-				)
-port map(
-				pixel_x => pixel_x,
-				pixel_y => pixel_y,
-				value => boxValues(12),
-				rgb_color => rgb13,
-				--this signal is so we can tell whether we should actually draw the color
-				--being output by the box or not!
-				drawBox => drawBox13
-);
+	generic map(
+						XPOS => 422,
+						YPOS => 244
+					)
+	port map(
+					pixel_x => pixel_x,
+					pixel_y => pixel_y,
+					value => boxValues(11),
+					rgb_color => rgb12,
+					--this signal is so we can tell whether we should actually draw the color
+					--being output by the box or not!
+					drawBox => drawBox12
+	);
 
 
-Box14 : entity work.Box 
+	Box13 : entity work.Box 
 
-generic map(
-					XPOS => 226,
-					YPOS => 342
-				)
-port map(
-				pixel_x => pixel_x,
-				pixel_y => pixel_y,
-				value => boxValues(13),
-				rgb_color => rgb14,
-				--this signal is so we can tell whether we should actually draw the color
-				--being output by the box or not!
-				drawBox => drawBox14
-);
+	generic map(
+						XPOS => 128,
+						YPOS => 342
+					)
+	port map(
+					pixel_x => pixel_x,
+					pixel_y => pixel_y,
+					value => boxValues(12),
+					rgb_color => rgb13,
+					--this signal is so we can tell whether we should actually draw the color
+					--being output by the box or not!
+					drawBox => drawBox13
+	);
 
 
-Box15 : entity work.Box 
+	Box14 : entity work.Box 
 
-generic map(
-					XPOS => 324,
-					YPOS => 342
-				)
-port map(
-				pixel_x => pixel_x,
-				pixel_y => pixel_y,
-				value => boxValues(14),
-				rgb_color => rgb15,
-				--this signal is so we can tell whether we should actually draw the color
-				--being output by the box or not!
-				drawBox => drawBox15
-);
+	generic map(
+						XPOS => 226,
+						YPOS => 342
+					)
+	port map(
+					pixel_x => pixel_x,
+					pixel_y => pixel_y,
+					value => boxValues(13),
+					rgb_color => rgb14,
+					--this signal is so we can tell whether we should actually draw the color
+					--being output by the box or not!
+					drawBox => drawBox14
+	);
 
-Box16 : entity work.Box 
 
-generic map(
-					XPOS => 422,
-					YPOS => 342
-				)
-port map(
-				pixel_x => pixel_x,
-				pixel_y => pixel_y,
-				value => boxValues(15),
-				rgb_color => rgb16,
-				--this signal is so we can tell whether we should actually draw the color
-				--being output by the box or not!
-				drawBox => drawBox16
-);
+	Box15 : entity work.Box 
+
+	generic map(
+						XPOS => 324,
+						YPOS => 342
+					)
+	port map(
+					pixel_x => pixel_x,
+					pixel_y => pixel_y,
+					value => boxValues(14),
+					rgb_color => rgb15,
+					--this signal is so we can tell whether we should actually draw the color
+					--being output by the box or not!
+					drawBox => drawBox15
+	);
+
+	Box16 : entity work.Box 
+
+	generic map(
+						XPOS => 422,
+						YPOS => 342
+					)
+	port map(
+					pixel_x => pixel_x,
+					pixel_y => pixel_y,
+					value => boxValues(15),
+					rgb_color => rgb16,
+					--this signal is so we can tell whether we should actually draw the color
+					--being output by the box or not!
+					drawBox => drawBox16
+	);
+	
+	
+	
 
 	
 
